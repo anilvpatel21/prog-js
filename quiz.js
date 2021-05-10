387cb70e53225a5f68803f376166e91ebf5ceeaa
Anilkumar Vinodbhai Patel <anilkumar.patel@flipkart.com>
2021-05-10 19:30:39 +0530

    switch case

--- quiz.js
+++ quiz.js
@@ -1,23 +1,35 @@
-var ans = 7;
+var ans1 = 7, ans2 = 7, ans3 = 8;
 var previousEle;
-function xyz(ele) {
+function xyz(ele, queNo) {
+    //alert(queNo);
     var parentEle = ele.parentElement;
     var grandEle = parentEle.parentElement;
-    // for(var child = 0; child < grandEle.children.length; child++) {
+    for(var child = 0; child < grandEle.children.length; child++) {
 
-    //     grandEle.children[child].classList.remove("bg-danger","bg-success");
+        grandEle.children[child].classList.remove("bg-danger","bg-success");
 
 
-    // }
-    if (previousEle) {
-        previousEle.classList.remove("bg-success", "bg-danger");
+    }
+    var answer; //container jo ki value store karenga
+    switch(queNo) {
+        case 1:
+            answer = ans1;
+            break;
+        case 2:
+            answer = ans2;
+            break;
+        case 3:
+            answer = ans3;
+            break;
+        default:
+            alert("Please select any one of the question");
     }
 
-    if (ele.value == ans) {
+    if (ele.value == answer) {
         parentEle.classList.add("bg-success");
     } else {
         parentEle.classList.add("bg-danger");
     }
 
-    previousEle = parentEle;
+
 }
